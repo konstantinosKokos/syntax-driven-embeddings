661ddcd467cda859b25c6e88c50b9a2cfe201132
stevenveerbeek <32898323+stevenveerbeek@users.noreply.github.com>
2017-10-22 12:14:21 +0200

    Updated matrix_tools.py
    Added method syntactic_update.

--- matrix_tools.py
+++ matrix_tools.py
@@ -30,53 +30,48 @@ def naive_update(matrix, vocabulary, parsed, window_size=3, ignore_unknown=True)
         
     return matrix
 
-def deptree_update_naive(matrix, vocabulary, parsed, symmetrical=False):
-	# deptree_update_naive: generates the context for a word by traversing up the dependency tree and adding all ancestors along the way.	
-	# Inputs:
-	# - symmetrical: if set to True, the dependency relations are stored symmetrically (i.e. if the head of word A is added to the dependency list of A, A itself is also added to the dependency list of its head)
+def syntactic_update(matrix, vocabulary, parsed, update_method, symmetrical=False):
 	if matrix.dtype==np.uint16: peak = 65535
 	for ii, token in enumerate(parsed):
 		lemma = token.lemma_
 		key = lemma
 		if key not in vocabulary.keys(): continue
 		index = vocabulary[key][2]
-		wordindexes = [] # indexes to change in the matrix
-		word = token
-		while word.dep_ != 'ROOT': # traverse up the dependency tree until you reach the root (the dependency of the root is 'ROOT')
-			word = word.head
-			wordkey = word.lemma_
-			wordindexes.append(vocabulary[wordkey][2])
+		wordindexes = update_method(vocabulary, token) # indexes to change in the matrix
 		for wordindex in wordindexes:
             if matrix[index, wordindex] < peak: matrix[index, wordindex] += 1
 			if symmetrical:
 				if matrix[wordindex, index] < peak: matrix[wordindex, index] += 1
     return matrix
 
-def deptree_update_headchildren(matrix, vocabulary, parsed):
-	# deptree_update_headchildren: generates the context for a word from its head plus its children (after Levy and Goldberg, 2014)
-	if matrix.dtype==np.uint16: peak = 65535
-	for ii, token in enumerate(parsed):
-		lemma = token.lemma_
-		key = lemma
-		if key not in vocabulary.keys(): continue
-		index = vocabulary[key][2]
-		wordindexes = [] # indexes to change in the matrix
-		if token.dep_ != 'prep':
-			for child in token.children:
-				dep_child = child.dep_
-				if dep_child == 'prep':
-					child = list(child.children)[0]
-					dep_child = 'prep_with'
-				childkey = child.lemma_
-				wordindexes.append(vocabulary[childkey][2])
-			head = token.head
-			dep = token.dep_
-			if dep == 'pobj':
-				head = head.head
-				dep = 'prep_with'
-			if dep != 'ROOT':
-				headkey = head.lemma_
-				wordindexes.append(vocabulary[headkey][2])
-		for wordindex in wordindexes:
-            if matrix[index, wordindex] < peak: matrix[index, wordindex] += 1
-    return matrix
+def deptree_naive(vocabulary, word):
+	# deptree_naive: generates the context for a word by traversing up the dependency tree and adding all ancestors along the way.	
+	# Inputs:
+	# - symmetrical: if set to True, the dependency relations are stored symmetrically (i.e. if the head of word A is added to the dependency list of A, A itself is also added to the dependency list of its head)
+	wordindexes = []
+	while word.dep_ != 'ROOT': # traverse up the dependency tree until you reach the root (the dependency of the root is 'ROOT')
+		word = word.head
+		wordkey = word.lemma_
+		wordindexes.append(vocabulary[wordkey][2])
+    return wordindexes
+
+def deptree_headchildren(vocabulary, token):
+	# deptree_headchildren: generates the context for a word from its head plus its children (after Levy and Goldberg, 2014)
+	wordindexes = []
+	if token.dep_ != 'prep':
+		for child in token.children:
+			dep_child = child.dep_
+			if dep_child == 'prep':
+				child = list(child.children)[0]
+				dep_child = 'prep_with'
+			childkey = child.lemma_
+			wordindexes.append(vocabulary[childkey][2])
+		head = token.head
+		dep = token.dep_
+		if dep == 'pobj':
+			head = head.head
+			dep = 'prep_with'
+		if dep != 'ROOT':
+			headkey = head.lemma_
+			wordindexes.append(vocabulary[headkey][2])
+    return wordindexes
