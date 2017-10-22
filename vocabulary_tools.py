0b48252bf7f1967b2e87e7e06a7c3ac1694ce866
konstantinosKokos <konstantinos@riseup.net>
2017-10-22 11:47:42 +0200

    added similarity methods

--- vocabulary_tools.py
+++ vocabulary_tools.py
@@ -41,49 +41,76 @@ def vocabularize(listfile, nlp, pos_decorated=True, lemmatized=True, return_hist
     if return_history: return vocabulary, history
     return vocabulary
 
+def mutualize(vocab_lem, vocab_keys, lemma_threshold=25, key_threshold=10, position=0, verbose=True, delete=True):
+    # mutualize: remove low occurence keys and lemmata from both dictionaries, to allow a 1:N correspondence between the two
+    # Inputs:
+    # - vocab_lem: the unreduced lemmata vocabulary
+    # - vocab_key: the unreduced keys vocabulary
+    # Outputs:
+    # - the set of the union of the unique lemmata in both stoplists
+    # Options:
+    # - lemma_threshold: minimum allowed occurence number for lemmata
+    # - key_threshold: minimum allowed occurence number for pos-decorated lemmata
+    # - position: 0 for TF, 1 for TDF
+    # - verbose: print through execution
+    # - delete: True to actually reduce the vocabularies, False to just produce the stoplist
+    
+    if verbose: print('...Cutting lemmata')
+    vocab_lem, low_occurence_lemmata = cut_frequency(vocab_lem, lemma_threshold, position, delete=delete, verbose=verbose)
+    if delete: vocab_keys = cut_stopwords(vocab_keys, low_occurence_lemmata, verbose)
+    if verbose: print('\n...Cutting keys')
+    vocab_keys, low_occurence_keys = cut_frequency(vocab_keys, key_threshold, position, delete=delete, verbose=verbose)
+    low_occ_keys_lemmatized = keys_to_lemmata(low_occurence_keys)
+    if delete: vocab_lem = cut_stopwords(vocab_lem, low_occ_keys_lemmatized)
+    return [x for x in set(low_occurence_lemmata + low_occ_keys_lemmatized)]
+
+def find_by_lemma(vocabulary, lemma, return_key=False):
+    for key in vocabulary.keys():
+        if key.split(' | ')[0] == lemma:
+            if return_key: yield key
+            else: yield vocabulary[key][2]
+
 def keys_to_lemmata(keys):
+    # keys_to_lemmata: remove the pos decoration from a string or list of strings
     if keys.__class__==str: 
         lemmata = keys.split(' | ')[0]
         return lemmata
     elif keys.__class__==list:
             lemmata = []
             for key in keys:
-                lemmata.append(key(' | ')[0])
-            return lemmata
-
+                lemmata.append(key.split(' | ')[0])
+            return lemmata    
+    
 def cut_stopwords(vocabulary, stopwords, verbose=True):
     # NEEDS FIXING
     for_deletion = []
     length_0 = float(len(vocabulary))
-    print('Cutting stoplist')
-    print('Initial size: ', len(vocabulary))
+    if verbose: print('Cutting stoplist')
+    if verbose: print('Initial size: ', len(vocabulary))
     for key in tqdm.tqdm_notebook(vocabulary):
         lemma = key.split(' | ')[0]
         if lemma in stopwords:
             for_deletion.append(key)
     for key in for_deletion: del vocabulary[key]
-    print('Final size: ', len(vocabulary))
-    print('Compression(%): ', (length_0 - len(vocabulary))/length_0)
+    if verbose: print('Final size: ', len(vocabulary))
+    if verbose: print('Compression(%): ', (length_0 - len(vocabulary))/length_0*100)
     return vocabulary
 
-def key_to_lemma(key):
-    lemma = []
-
-def cut_frequency(vocabulary, threshold, position=0, no_delete=True, verbose=True):
+def cut_frequency(vocabulary, threshold, position=0, delete=True, verbose=True):
     if verbose: print('Deleting entries with position', position, 'value less than', threshold)
     length_0 = float(len(vocabulary))
     reduction = 0
     if verbose: print('Initial size: ', len(vocabulary))
     for_deletion = []
-    for key in vocabulary:
+    for key in vocabulary.keys():
         if vocabulary[key][position] < threshold: for_deletion.append(key)
     for key in for_deletion: 
         reduction +=1
-        if not no_delete: del vocabulary[key]
+        if delete: del vocabulary[key]
     if verbose: 
         print('Final size: ', length_0-reduction)
         print('Compression(#): ', reduction)
-        print('Compression(%): ', (reduction/length_0))
+        print('Compression(%): ', (reduction/length_0)*100)
     return vocabulary, for_deletion
 
 def cut_top_edge(vocabulary, threshold=0.35, position=0, no_delete=False, verbose=True):
@@ -123,6 +150,21 @@ def indexize(vocabulary, index=0):
         vocabulary[key] = temp
     return vocabulary
 
-        
-    
+def reverse(vocabulary):
+    '''
+    map from indices to keys
+    '''
+    temp = indexize(vocabulary)
+    reverse_vocab = {}
+    for key in temp.keys():
+        reverse_vocab[temp[key][2]]=key
+    return reverse_vocab
 
+def idx_to_words(indices, rev):
+    '''
+    map from list of indices to list of words
+    '''
+    words = []
+    for idx in indices:
+        words.append(rev[idx])
+    return words
