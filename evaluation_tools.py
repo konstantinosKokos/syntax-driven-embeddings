8f5c5c515b6b9e9ef0865221f156a338f6ad4091
konstantinosKokos <konstantinos@riseup.net>
2017-10-28 19:28:15 +0200

    added bless methods

--- evaluation_tools.py
+++ evaluation_tools.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import csv 
 import copy
+from matrix_tools import pair_similarity
+import numpy as np
+from tqdm import tqdm_notebook
 
-def load(filepath='/home/jovyan/kokos-playground/Data/TOEFL.csv'):
+def construct_toefl(filepath='/home/jovyan/kokos-playground/Data/TOEFL.csv'):
     '''
     description goes here
     '''
@@ -13,7 +16,7 @@ def load(filepath='/home/jovyan/kokos-playground/Data/TOEFL.csv'):
     tests = pd.DataFrame(data=data, columns=['original','ground_truth','test_1','test_2','test_3'])
     return tests
 
-def reduce(tests, minimum, vocabulary):
+def reduce_toefl(tests, minimum, vocabulary):
     '''
     description goes here
     '''
@@ -29,7 +32,7 @@ def reduce(tests, minimum, vocabulary):
         if badword: to_delete.append(ii)
     reduced = reduced.drop(reduced.index[to_delete])
     return reduced
-
+        
 def multifold_test(tests, similarities, vocabulary, split=10):
     '''
     description goes here
@@ -59,4 +62,105 @@ def multifold_test(tests, similarities, vocabulary, split=10):
     results[split-1] = (batch_size-num_errors)/batch_size
     return results, batch_sizes, num_errorss
     
+def construct_bless(pathfile, vocabulary, similarities):
+    '''
+    description goes here
+    '''
+    with open(pathfile, 'r') as f:
+        z = csv.reader(f, delimiter='\t')
+        data = []
+        for row in z: 
+            newrow = []
+            newrow.append(row[0].split('-')[0])
+            newrow.append(row[2])
+            newrow.append(row[3].split('-')[0])
+            newrow.append(0)
+            data.append(newrow)
+    bless = pd.DataFrame(data=data, columns=['root','category','word', 'score'])
+    bless = reduce_bless(bless, vocabulary)
+    bless['score'] = bless['score'].astype(float)
+    for ii in tqdm_notebook(range(len(bless))):
+        bless.set_value(ii, 'score', pair_similarity(bless.iloc[ii]['root'], bless.iloc[ii]['word'], vocabulary, similarities))
+    return bless
+
+def reduce_bless(bless, vocabulary):
+    '''
+    description goes here
+    '''
+    to_delete = []
+    for ii in range(len(bless)):
+        if bless.iloc[ii]['root'] not in vocabulary.keys() or vocabulary[bless.iloc[ii]['root']][0]< 40 or bless.iloc[ii]['word'] not in vocabulary.keys(): to_delete.append(ii)
+    bless = bless.drop(bless.index[to_delete])
+    bless.index = [i for i in range(len(bless))]
+    return bless
+
+def summarize(bless):
+    '''
+    description goes here
+    '''
+    words = {}
+    attributes = []
+    coords = []
+    events = []
+    hypers = []
+    meros = []
+    randomjs = []
+    randomvs = []
+    randomns = []
+    currentword = bless.iloc[0]['root']
+    for ii in range(len(bless)):
+        if currentword != bless.iloc[ii]['root']: 
+            words[currentword] = [attributes, coords, events, meros, hypers, randomjs, randomvs, randomns]
+            attributes = []
+            coords = []
+            events = []
+            hypers = []
+            meros = []
+            randomjs = []
+            randomvs = []
+            randomns = []
+            currentword = bless.iloc[ii]['root']
+        if bless.iloc[ii]['category'] == 'mero': meros.append(bless.iloc[ii]['score'])
+        if bless.iloc[ii]['category'] == 'hyper': hypers.append(bless.iloc[ii]['score'])
+        if bless.iloc[ii]['category'] == 'attri': attributes.append(bless.iloc[ii]['score'])
+        if bless.iloc[ii]['category'] == 'coord': coords.append(bless.iloc[ii]['score'])
+        if bless.iloc[ii]['category'] == 'event': events.append(bless.iloc[ii]['score'])  
+        if bless.iloc[ii]['category'] == 'random-j': randomjs.append(bless.iloc[ii]['score'])
+        if bless.iloc[ii]['category'] == 'random-v': randomvs.append(bless.iloc[ii]['score'])
+        if bless.iloc[ii]['category'] == 'random-n': randomns.append(bless.iloc[ii]['score'])
+    words[currentword] = [attributes, coords, events, meros, hypers, randomjs, randomvs, randomns]
+
+    '''for word in words:
+        centered_vectors = []
+        for vector in words[word]:
+            centered_vectors.append(center_and_mean(vector))
+        words[word] = centered_vectors
+    '''
     
+    for word in words:
+        vector = []
+        for ii in range(len(words[word])):
+            newvalue = max(words[word][ii])
+            vector.append(newvalue)
+        words[word] = vector
+    '''
+    attributes = [max(words[word][0]) for word in words]
+    coords = [max(words[word][1]) for word in words]
+    events = [max(words[word][2]) for word in words]
+    meros = [max(words[word][3]) for word in words]
+    hypers = [max(words[word][4]) for word in words]
+    randomjs = [max(words[word][5]) for word in words]
+    randomvs = [max(words[word][6]) for word in words]
+    randomns = [max(words[word][7]) for word in words]
+    
+    for ii, word in enumerate(words):
+        vector = [attributes[ii], coords[ii], events[ii], meros[ii], hypers[ii], randomjs[ii], randomvs[ii], randomns[ii]]
+        vector = center_and_mean(vector)
+        words[word] = vector
+    '''
+    
+    return words#, attributes, coords, events, meros, hypers, randoms
+            
+def center_and_mean(vector):
+    vector = vector - np.mean(vector)
+    return vector/ np.std(vector, ddof=1)
